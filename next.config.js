cc3bea923cde98bc04439adc808f3195a450ce99
gaetanmus <gaetanmus@gaetanmus>
2023-09-10 06:54:42 +0200

    commit 1

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-     images: { unoptimized: true } ,
-    // reactStrictMode: true,
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`,
-   //trailingSlash: true,
- 
- // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  //skipTrailingSlashRedirect: true,
- 
-  //Optional: Change the output directory `out` -> `dist`
-   //distDir: 'dist',
-   distDir: '_static',
-}
+const nextConfig = {}
 
 module.exports = nextConfig
